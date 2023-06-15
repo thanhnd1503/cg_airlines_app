c49b47f817a67dced6b44ec318aa889db5340784
Trân Nguyễn <huyentrantm.12345@gmail.com>
2023-06-15 14:56:30 +0700

    thij binh

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 // import "./../src/asset/css/main.css";
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import SignUpForm from './component/SignUpForm';
-import ErrorPage from './component/errorPage';
-import LoginForm from './component/LoginForm';
-import DashBoard from './component/DashBoard';
-import {tokenLoader} from "../src/utility/author";
-// <<<<<<< HEAD
-// import TripListContent from "../src/component/Ticket/ticketBooking";
-// =======
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUpForm from "./component/SignUpForm";
+import ErrorPage from "./component/errorPage";
+import LoginForm from "./component/LoginForm";
+import DashBoard from "./component/DashBoard";
+import { tokenLoader } from "../src/utility/author";
+import ViewTicket from "./component/Ticket/viewTicket";
+import Eticket from "./component/Ticket/electronicTicket";
+import TicketBooking from "./component/Ticket/ticketBooking";
+
 import TripListContent from "./component/Ticket/ticketBooking";
-import PassengerInfomation from './component/PassengerInfomation';
-
-// >>>>>>> 7acfda6722a12512a7800b7c2861e462f1a1ae77
-
+import PassengerInfomation from "./component/PassengerInfomation";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<DashBoard/>} errorElement={<ErrorPage/>} loader={{tokenLoader}}>
-    </Route>
-    <Route path={`/search-result/:departure/:destination/:departureDate/:ticketClass`}
-                           element={<TripListContent/>}></Route>
-{/*<<<<<<< HEAD*/}
-{/*                    <Route path='/login' element={<LoginForm/>}></Route>*/}
-{/*                    <Route path='/register' element={<SignUpForm/>}></Route></Routes>*/}
-{/*            </BrowserRouter>*/}
-{/*=======*/}
-    <Route path='/login' element={<LoginForm/>}></Route>
-    <Route path='/register' element={<SignUpForm/>}></Route>
-    <Route path='/passengerInfo' element={<PassengerInfomation/>}></Route></Routes>
-    </BrowserRouter>
-{/*>>>>>>> 7acfda6722a12512a7800b7c2861e462f1a1ae77*/}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<DashBoard />}
+            errorElement={<ErrorPage />}
+            loader={{ tokenLoader }}
+          ></Route>
+          <Route
+            path={`/search-result/:departure/:destination/:departureDate/:ticketClass`}
+            element={<TripListContent />}
+          ></Route>
 
-        </>
-    );
+          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/register" element={<SignUpForm />}></Route>
+          <Route
+            path="/passengerInfo"
+            element={<PassengerInfomation />}
+          ></Route>
+          <Route path="/viewTicket" element={<ViewTicket />}></Route>
+          <Route path="/eTicket" element={<Eticket />}></Route>
+          <Route path="/ticketBooking" element={<TicketBooking/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
