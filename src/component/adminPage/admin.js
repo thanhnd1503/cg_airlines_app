596e8c45c4ceffd22441811448047469d1ca0b6a
binh113 <quocbinh230597@gmail.com>
2023-06-18 07:39:15 +0700

    add admin.css

--- src/component/adminPage/admin.js
+++ src/component/adminPage/admin.js
@@ -3,21 +3,17 @@ import Sidebar from "./sidebar/Sidebar";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import '../../asset/css/SearchResult.css';
-import { current } from "@reduxjs/toolkit";
 import NavBar from "../adminPage/navbar/Navbar"
 import "../../asset/css/admin.css"
-import tripContent from "./trip/tripContent";
+import TripList from "./trip/TripList";
 export default function Admin() {
   return (
     <>
-        <div>
         <NavBar></NavBar>
         <div id="layoutSidenav">
-
         <Sidebar />
-        <tripContent></tripContent>
+        <TripList/>
           </div>
-        </div>
     </>
   );
 }
